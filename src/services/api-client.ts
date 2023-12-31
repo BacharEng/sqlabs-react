c543b3a841fa50374c27f2f701f215826cce912e
Sagi Bachar Adler <sagienator@gmail.com>
2023-12-31 17:21:33 +0200

    Update api-client.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: '53689621a64a4ef0a344830606c7b39f'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "85989caec4f3477bb41c4bc63084c0f7",
+  },
+});
